2dafe494034c1ace9cd6a752e05a3d478c646606
David Dong <daviddong201612@gmail.com>
2021-05-18 13:11:39 -0500

    2

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile__user-panel">
+      <h1 class="user-profile__username">@{{ user.username }}</h1>
+      <div class="user-profile__follower-count">
+        <strong>Followers: </strong>{{ followers }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserProfile",
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "_DavidDong",
+        firstName: "David",
+        lastName: "Dong",
+        email: "daviddong201612@gmail.com",
+        isAdmin: true
+      }
+    };
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower!`);
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    }
+  },
+  mounted() {
+    this.followUser();
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
