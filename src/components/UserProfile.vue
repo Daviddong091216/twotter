edf15be8ab4c7f8cd1828f18d35f102793f0ee35
David Dong <daviddong201612@gmail.com>
2021-05-18 22:33:15 -0500

    3

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -2,26 +2,60 @@
   <div class="user-profile">
     <div class="user-profile__user-panel">
       <h1 class="user-profile__username">@{{ user.username }}</h1>
+      <!-- v-if and v-else conditional -->
+      <div class="user-profile__admin-badge" v-if="user.isAdmin">
+        Admin
+      </div>
+      <div class="user-profile__admin-badge" v-else>
+        Common User
+      </div>
       <div class="user-profile__follower-count">
         <strong>Followers: </strong>{{ followers }}
       </div>
+      <div class="user-profile__favourite">
+          {{favouritedTwoot}}
+      </div>
+      <form class="user-profile__create-twoot">
+          <label for="newTwoot"><strong>New Twoot</strong></label>
+          <textarea id="newTwoot" rows="4"/>
+      </form>
+    </div>
+    <div class="user-profile__twoots-wrapper">
+        <!-- v-for loop and data passing -->
+         <!-- @favourite="toggleFavourite" capture the emit date -->
+      <TwootItem
+        v-for="twoot in user.twoots"
+        :key="twoot.id"
+        :username="user.username"
+        :twoot="twoot"
+        @favourite="toggleFavourite" 
+      />
     </div>
   </div>
 </template>
 
 <script>
+import TwootItem from "./TwootItem";
 export default {
   name: "UserProfile",
+  components: { TwootItem },
   data() {
     return {
       followers: 0,
+      favouritedTwoot:"",
       user: {
         id: 1,
         username: "_DavidDong",
         firstName: "David",
         lastName: "Dong",
         email: "daviddong201612@gmail.com",
-        isAdmin: true
+        isAdmin: false,
+        twoots: [
+          { id: 1, content: "Learing Vue.js" },
+          { id: 2, content: "Learing NodeJs" },
+          { id: 3, content: "Learing Java" },
+          { id: 4, content: "Learing MySQL" }
+        ]
       }
     };
   },
@@ -40,6 +74,9 @@ export default {
   methods: {
     followUser() {
       this.followers++;
+    },
+    toggleFavourite(id){
+        this.favouritedTwoot=`My favorite twoot's id is ${id}`
     }
   },
   mounted() {
@@ -49,14 +86,45 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+.user-profile {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  width: 100%;
+  padding: 50px 5%;
+}
+.user-profile__user-panel {
   display: flex;
   flex-direction: column;
-  color: #2c3e50;
-  /* margin-top: 60px; */
+  margin-right: 50px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px sold #dfe3e8;
+}
+
+.user-profile__admin-badge {
+  background: red;
+  color: white;
+  border-radius: 5px;
+  margin-right: auto;
+  padding: 0 10px;
+  font-weight: bold;
+}
+
+.user-profile__favourite {
+  background:darkgreen;
+  color: white;
+  border-radius: 5px;
+  margin-right: auto;
+  padding: 0 20px;
+ }
+.user-profile__create-twoot{
+    padding-top:20px;
+    display: flex;
+    flex-direction: column;
+}
+
+h1 {
+  margin: 0;
 }
 </style>
